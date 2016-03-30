9f66746f9b85eb7244130a8d5080ce03c6511052
hohoTT <609029365@qq.com>
2016-03-30 22:07:35 +0800

    添加save方法获取值栈，并添加栈顶元素

---
+++ SrustsOGNLDemo/src/com/wt/valueStack/Test.java
@@ -0,0 +1,23 @@
+package com.wt.valueStack;
+
+public class Test {
+	
+	private String productName;
+	private String productDesc;
+	
+	
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getProductDesc() {
+		return productDesc;
+	}
+	public void setProductDesc(String productDesc) {
+		this.productDesc = productDesc;
+	}
+
+	
+}
